11d8100bc0577661917fd1fd04e4b49143bc22b1
Angra Santos <angra.santos@hotmail.com.br>
2022-05-05 22:21:28 -0300

    Initial commit

---
+++ src/Aplicacao/Switch1.java
@@ -0,0 +1,20 @@
+package Aplicacao;
+
+public class Switch1 {
+    public static void main(String[] args) {
+        //Realização de um switch sem break
+
+        int idade = 5;
+        switch(idade){
+            case 3:
+                System.out.println("Sabe programar!");
+            case 2:
+                System.out.println("Sabe falar!");
+            case 1:
+                System.out.println("Sabe andar!");
+            case 0:
+                System.out.println("sabe respirar!");
+        }
+
+    }
+}
